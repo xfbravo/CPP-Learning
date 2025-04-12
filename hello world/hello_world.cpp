a660542b503bb4402481d31694cc8463c41e8c21
xfbrabo <2102356261@qq.com>
2025-04-12 21:59:04 +0800

    delete all

--- hello world/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Hello, World!--by xf" << endl;
-    return 0;
-}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("This is a test program.\n");
-    printf("Let's see if it works!\n");
-    return 0;
-}
